d839dae9e13b516fbc4eb5161f22dcff02c121b9
MyUsername <my@email.com>
2022-12-23 20:36:50 +0100

    "La mia prima app con Vite!"

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+
+   <section>
+      <div>
+         <h1>
+            "La mia prima app con Vite!"
+         </h1>
+      </div>
+   </section>
+
+</template>
+
+<style>
+
+</style>
